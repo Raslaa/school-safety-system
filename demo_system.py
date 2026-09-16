ddab825b5af940c6713311d7e805ebc4e629b3ff
Raslaa <124137757+Raslaa@users.noreply.github.com>
2026-09-16 16:13:57 +0300

    Update parent_1 phone number

--- demo_system.py
+++ demo_system.py
@@ -13,7 +13,7 @@ name = "Ahmed Mohamed"
 photo = "ahmed.jpg"
 bio = "Friendly and active student. Loves football and reading."
 allergies = "Peanuts"
-parent_1 = "+252 63 1234567"
+parent_1 = "+252 63 4403028"
 parent_2 = "+252 63 7654321"
 
 
